35adc42d03373689d708d74d78a5f63afc46af60
Eric Renard <timmy469@gmail.com>
2018-09-14 19:10:52 +0200

    Optimize only in production

--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,71 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-module.exports = (env, argv) => ({
-  entry: { main: './src/main' },
-  output: {
-    filename: '[name].[hash].js',
-    path: path.join(__dirname, 'dist')
-  },
-  resolve: {
-    modules: [
-      'src',
-      'node_modules'
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html' }),
-    new CleanWebpackPlugin('dist', { exclude: ['.git', '.circleci', '.gitignore'] })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.glsl$/,
-        use: 'raw-loader'
-      },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      {
-        test: /\.(png|jpg)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[hash].[ext]'
-          }
-        }
-      },
-      { test: /\.ttf$/, use: 'file-loader' }
-    ]
-  },
-  devtool: 'source-map',
-  devServer: {
-    compress: false,
-    port: 9000
-  },
-  optimization: {
-    minimize: argv.mode === 'production',
-    runtimeChunk: {
-      name: 'vendor'
+module.exports = (env, argv) => {
+  const config = {
+    entry: { main: './src/main' },
+    output: {
+      filename: '[name].[hash].js',
+      path: path.join(__dirname, 'dist')
     },
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        commons: {
-          test: /node_modules/,
-          name: 'vendor',
-          chunks: 'initial',
-          minSize: 1
+    resolve: {
+      modules: [
+        'src',
+        'node_modules'
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: './src/index.html' }),
+      new CleanWebpackPlugin('dist', { exclude: ['.git', '.circleci', '.gitignore'] })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.glsl$/,
+          use: 'raw-loader'
+        },
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        {
+          test: /\.(png|jpg)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]'
+            }
+          }
+        },
+        { test: /\.ttf$/, use: 'file-loader' }
+      ]
+    },
+    devtool: 'source-map',
+    devServer: {
+      compress: false,
+      port: 9000
+    }
+  }
+  if (argv.mode === 'production') {
+    config.optimization = {
+      minimize: argv.mode === 'production',
+      runtimeChunk: {
+        name: 'vendor'
+      },
+      splitChunks: {
+        cacheGroups: {
+          default: false,
+          commons: {
+            test: /node_modules/,
+            name: 'vendor',
+            chunks: 'initial',
+            minSize: 1
+          }
         }
       }
     }
   }
-})
+  return config
+}
