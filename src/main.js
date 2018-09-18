b66e571dede0e44b65a9c456dbfb087d3a82c394
Eric Renard <timmy469@gmail.com>
2018-09-18 08:54:50 +0200

    use of pixi-viewport

--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
 import 'normalize-css'
 import Resources from './resources'
 import * as PIXI from 'pixi.js'
+import Viewport from 'pixi-viewport'
 
-new Promise(resolve => {
-  const app = new PIXI.Application(640, 640)
-  document.body.appendChild(app.view)
-  Resources.addResourcesToLoader(app.loader)
-  app.loader.load(resolve(app))
-}).then(app => {
-  const texture = PIXI.Texture.from(app.loader.resources.paletteHue.data)
+const viewportSideDimension = 640
+
+function createMandelbrotQuad (min, max) {
   const geometry = new PIXI.Geometry()
     .addAttribute('aVertexPosition', // the attribute name
       [
-        -100, -100, // x, y
-        100, -100, // x, y
-        100, 100,
-        -100, 100 // x, y
+        min, min, // x, y
+        max, min, // x, y
+        max, max,
+        min, max // x, y
       ],
       2) // the size of the attribute
     .addAttribute('aTexturePosition', // the attribute name
@@ -29,6 +26,29 @@ new Promise(resolve => {
     .addIndex([0, 1, 2, 0, 2, 3])
     .interleave()
 
+  return geometry
+}
+
+new Promise(resolve => {
+  const app = new PIXI.Application(viewportSideDimension, viewportSideDimension)
+  document.body.appendChild(app.view)
+  Resources.addResourcesToLoader(app.loader)
+  app.loader.load(resolve(app))
+}).then(app => {
+  const viewport = new Viewport({
+    screenWidth: app.screen.width,
+    screenHeight: app.screen.height,
+    worldWidth: viewportSideDimension,
+    worldHeight: viewportSideDimension
+  })
+  viewport.drag().pinch()
+    .wheel()
+    .decelerate()
+
+  app.stage.addChild(viewport)
+
+  const geometry = createMandelbrotQuad(0, viewportSideDimension)
+  const texture = PIXI.Texture.from(app.loader.resources.paletteHue.data)
   const shader = PIXI.Shader
     .from(
       Resources.shaders.mandelbrotVertex,
@@ -37,12 +57,6 @@ new Promise(resolve => {
         colorPalette: texture,
         maxIteration: 64
       })
-
   const square = new PIXI.Mesh(geometry, shader)
-  square.position.set(app.screen.width / 2, app.screen.height / 2)
-  app.stage.addChild(square)
-  square.scale.set(2)
-  app.ticker.add(function (delta) {
-    //    square.rotation += 0.01
-  })
+  viewport.addChild(square)
 })
